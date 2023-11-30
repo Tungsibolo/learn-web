7175d80ef4afe3cc9422dd62aed7e5ea65b0acaf
Tung Do <tungsibolo2003@gmail.com>
2023-11-30 16:58:10 +0700

    commit lan 7 30/11/2023

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,49 +1,65 @@
-import db from '../models/index';
+import db from "../models/index";
 import CRUDService from "../services/CRUDService";
-let getHomePage = async (req,res) => {
-    try{
-        let data = await db.User.findAll();
-        console.log('---------------')
-        console.log(data)
-        console.log('---------------')
-        return res.render('homepage.ejs', {
-            data: JSON.stringify(data)
-        });
-        
-    }catch(e) {
-        console.log(e)
-    }
-    
-}
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    console.log("---------------");
+    console.log(data);
+    console.log("---------------");
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-let getAboutPage = (req,res) => {
-    return res.render('test/about.ejs');
-}
+let getAboutPage = (req, res) => {
+  return res.render("test/about.ejs");
+};
 
-let getCRUD = (req,res) => {
-    return res.render('crud.ejs');
-}
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
 
-let postCRUD = async (req,res) => {
-    let message = await CRUDService.createNewUser(req.body);
-    console.log(message)
-    return res.send('post crud from server');
-}
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud from server");
+};
 
-let displayGetCRUD = async (req,res) => {
-    let data = await CRUDService.getAllUser();
-    console.log('----------------')
-    console.log(data)
-    console.log('----------------')
-    return res.render('displayCRUD.ejs', {
-        dataTable: data
-    })
-}
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  console.log("----------------");
+  console.log(data);
+  console.log("----------------");
+  return res.render("displayCRUD.ejs", {
+    dataTable: data,
+  });
+};
+
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  let userData = await CRUDService.getUserInfoById(userId);
+  return res.render("editCRUD.ejs", {
+    user: userData,
+  });
+};
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render("displayCRUD.ejs", {
+    dataTable: allUsers,
+  });
+};
 
 module.exports = {
-    getHomePage : getHomePage,
-    getAboutPage : getAboutPage,
-    getCRUD: getCRUD,
-    postCRUD: postCRUD,
-    displayGetCRUD: displayGetCRUD,
-}
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
+};
