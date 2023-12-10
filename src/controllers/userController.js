bc2ff75d5eb4344c655e75cb44975b3aef58e9ed
Tung Do <tungsibolo2003@gmail.com>
2023-12-10 20:33:14 +0700

    commit lan 10 10/12/2023

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,6 +20,26 @@ let handleLogin = async (req, res) => {
   });
 };
 
+let handleGetAllUsers = async (req, res) => {
+  let id = req.query.id;
+
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Misssing required parameters",
+      users: [],
+    });
+  }
+
+  let users = await userService.getAllUsers(id);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "OK",
+    users,
+  });
+};
+
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
 };
